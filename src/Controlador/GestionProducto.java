060e9d7046434265e61b5a4a664fb6882dbb9d22
LENOVO <LENOVO@DESKTOP-9862MK8>
2018-01-26 00:28:40 -0500

    Termine de crear GestionProducto con todas sus herencias

--- src/Controlador/GestionProducto.java
+++ src/Controlador/GestionProducto.java
@@ -6,13 +6,72 @@
 package Controlador;
 
 
+
+import Modelo.Producto;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  *
  * @author LENOVO
  */
-public class GestionProducto implements OperacionesProduto{
+public class GestionProducto implements OperacionesCrudProducto{
    private static ArrayList<Producto> registroProducto;
+
+    public GestionProducto() {
+        
+        registroProducto = new ArrayList<>();
+    }
+
+   
+   
+   
+   
+    @Override
+    public Boolean registrarProducto(Producto p) {
+        return registroProducto .add(p);
+    }
+
+    @Override
+    public Boolean actualizarProducto(Producto P) {
+       for (int i = 0; i<registroProducto.size();i++){
+         if(registroProducto.get(i).getCodigoproducto().equals(P.getCodigoproducto())) {
+             registroProducto.set(i, P);
+             return true;
+         }  
+        }
+       return false;
+    }
+
+    @Override
+    public Boolean eliminarProducto(Integer numiden) {
+       for (int i = 0 ; i< registroProducto.size();i++){
+           Object Codigoproducto = null;
+            if (Objects.equals( registroProducto.get(i).getCodigoproducto(),Codigoproducto )){
+                 registroProducto.remove(i);
+                return true;
+            }
+        }
+       return false;
+    }
+
+    @Override
+    public Producto buscarProducto(Integer numiiden) {
+        for (Producto P :registroProducto ) {
+            Object Codigoproducto = null;
+         if(P.getCodigoproducto().equals(Codigoproducto)) {
+             return P;
+         }  
+            
+        }
+       return null;  
+
+    }
+
+    @Override
+    public ArrayList<Producto> mostrartodo() {
+         return registroProducto;
+
+    }
  
 }
